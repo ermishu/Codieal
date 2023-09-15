36165735fbb15b801b4cfb3e39e5c3530631d2e9
Shruti <er.shrutimishra.2002@gmail.com>
2023-09-15 13:26:22 +0530

    Adding Database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
+const db =require('./config/mongoose')
 
 // use static file 
 app.use(express.static('./assets'))
